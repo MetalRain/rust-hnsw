79ab0d85d84d68f9d894218249c94320f08c1216
Otto Martikainen <martikainenotto@gmail.com>
2023-07-05 09:57:08 +0300

    Add normalization step

--- readme.md
+++ readme.md
@@ -13,3 +13,6 @@ https://huggingface.co/learn/nlp-course/chapter6/8?fw=pt
 Hierarchical Navigable Small World:
 https://www.pinecone.io/learn/series/faiss/hnsw
 
+
+Rust & Webassembly:
+https://rustwasm.github.io/docs/book/introduction.html
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,8 +1,12 @@
+extern crate unicode_normalization;
+
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
 use std::convert::AsRef;
 use std::path::Path;
 use std::vec::Vec;
+
+use unicode_normalization::UnicodeNormalization;
 
 
 fn read_lines<P>(path: P) -> io::Result<Lines<BufReader<File>>>
@@ -12,15 +16,27 @@ fn read_lines<P>(path: P) -> io::Result<Lines<BufReader<File>>>
     Ok(BufReader::new(file).lines())
 }
 
+fn normalize(line: String) -> String {
+    line
+        // unicode normalization
+        .as_str().nfc().to_string()
+        //TODO: accent normalization
+        // case normalization
+        .to_lowercase()
+}
+
 fn load_corpus_file(name: &str) -> impl Iterator<Item = String> {
     let filename = "./data/".to_owned() + name;
     
     read_lines(filename)
         .expect("Could not find the file")
         .map(|l| l.unwrap())
+        .map(|l| normalize(l))
 }
+
 
 pub fn update_tokenizer() {
     let lines = load_corpus_file("t8.shakespeare.txt");
+
     println!("Lines {}", lines.collect::<Vec<String>>().len());
 }
