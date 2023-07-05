5d4138c72cb44ce8118836f675488d87541706ad
Otto Martikainen <martikainenotto@gmail.com>
2023-07-05 10:32:54 +0300

    Add whitespace pretokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -16,8 +16,8 @@ fn read_lines<P>(path: P) -> io::Result<Lines<BufReader<File>>>
     Ok(BufReader::new(file).lines())
 }
 
-fn normalize(line: String) -> String {
-    line
+fn normalize(phrase: String) -> String {
+    phrase
         // unicode normalization
         .as_str().nfc().to_string()
         //TODO: accent normalization
@@ -25,18 +25,26 @@ fn normalize(line: String) -> String {
         .to_lowercase()
 }
 
-fn load_corpus_file(name: &str) -> impl Iterator<Item = String> {
+fn pre_tokenize(phrase: String) -> Vec<String> {
+    // TODO: how can this return Iterator of &str?
+    phrase.split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+}
+
+fn load_corpus_file(name: &str) -> impl Iterator<Item = Vec<String>> {
     let filename = "./data/".to_owned() + name;
-    
+
     read_lines(filename)
         .expect("Could not find the file")
         .map(|l| l.unwrap())
-        .map(|l| normalize(l))
+        .map(|phrase| normalize(phrase))
+        .map(|phrase| pre_tokenize(phrase))
 }
 
 
 pub fn update_tokenizer() {
     let lines = load_corpus_file("t8.shakespeare.txt");
 
-    println!("Lines {}", lines.collect::<Vec<String>>().len());
+    println!("Lines {}", lines.collect::<Vec<_>>().len());
 }
